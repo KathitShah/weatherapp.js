145704a7bec53ebca0f1770c3c2651c4e1c9d0a6
KathitShah <49078541+KathitShah@users.noreply.github.com>
2020-07-15 10:42:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -18,61 +18,6 @@ function getResults(query) {
     })
     .then(displayResults);
 }
-function startResults() {
-  fetch(`${api.base}weather?q=winnipeg&units=metric&APPID=${api.key}`)
-    .then((weather) => {
-      return weather.json();
-    })
-    .then(displayResults2);
-}
-function displayResults2(weather) {
-  let city = document.querySelector(".location .city");
-  city.innerText = `${weather.name}, ${weather.sys.country}`;
-  let now = new Date();
-  let date = document.querySelector(".location .date");
-  date.innerText = dateBuilder2(now);
-  let temp = document.querySelector(".current .temp");
-  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-  let weather_el = document.querySelector(".current .weather");
-  weather_el.innerText = weather.weather[0].main;
-  let hilow = document.querySelector(".current .hi-low");
-  hilow.innerHTML = `${Math.round(weather.main.temp_min)}°c / ${Math.round(
-    weather.main.temp_max
-  )}°c`;
-  searchbox.value = "";
-}
-
-function dateBuilder2(d) {
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let days = [
-    "",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-  return `${day} ${date} ${month} ${year}`;
-}
 function displayResults(weather) {
   let city = document.querySelector(".location .city");
   city.innerText = `${weather.name}, ${weather.sys.country}`;
@@ -120,31 +65,4 @@ function dateBuilder(d) {
   let month = months[d.getMonth()];
   let year = d.getFullYear();
   return `${day} ${date} ${month} ${year}`;
-}
-function startTime() {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  pam = "am";
-  if (h > 12) {
-    pam = "pm";
-    h = h - 12;
-  } else if (h === 12) {
-    pam = "pm";
-  }
-  document.getElementById("txt").innerHTML = h + ":" + m + ":" + s + " " + pam;
-  var t = setTimeout(startTime, 500);
-}
-function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i;
-  } // add zero in front of numbers < 10
-  return i;
-}
-function starterOnLoad(){
-  startTime();
-  startResults();
 }
